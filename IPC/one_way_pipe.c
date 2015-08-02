692b4679b5bbfc9c6b3afb2784edf628e60479b3
Akash <p.akashchaturvedi@gmail.com>
2015-08-01 20:11:03 -0500

    uses a simple pipe to read and write data between two processes

--- IPC/one_way_pipe.c
+++ IPC/one_way_pipe.c
@@ -1,6 +1,35 @@
 //this program shows implementation of one way IPC between n processes
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-void main()
+int main()
 {
-	pritnf("this is process 1");
+        pid_t pid;
+        int myPipeFd[2];
+        int ret;
+        char buf[20];
+
+        ret = pipe(myPipeFd);
+
+        if(ret == -1){
+                perror("pipe");
+                exit(1);
+        }
+
+        pid = fork();
+
+        if(pid == 0)    //child
+        {
+                printf("[PARENT PROCESS]\n");
+                read(myPipeFd[0], buf, 15);
+                printf("read message is: %s", buf);
+        }else{
+                //parent process
+
+                printf("[CHILD PROCESS]\n\n");
+                write(myPipeFd[1], "Heyy there", 12);//first integer in the array is set up and opened for reading, while the second integer for writing.
+        }
+        return 0;
 }
+
