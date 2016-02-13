e7a98702c691767f814e78e8e1408d487415a1f5
Akash Chaturvedi <axc144430@utdallas.edu>
2016-02-13 14:45:21 -0600

    added separate diretory for multithreading and thread synchronization stuff

--- general/BST.c
+++ general/BST.c
@@ -1,2 +1,65 @@
 //binary search tree implementation in C
+#include <stdio.h>
+#include <malloc.h>
 
+struct node {
+	int data;
+	struct node* left;
+	struct node* right;
+};
+
+void inorder_traverse(struct node* node){
+	if(node->left != NULL){
+		inorder_traverse(node->left);
+	}
+
+	printf(" -> %d",node->data);
+	
+	if(node->right != NULL){
+		inorder_traverse(node->right);
+	}
+}
+
+void insert_node(int data, struct node* start){
+	if(start->left == NULL )
+
+	if(data < start->data){
+		//also have to check start->left is not null i.e
+		if(start->left != null){
+			insert_node(data, start->left);
+		} else {
+			start->left = ())
+		}
+	}
+	else {
+		//also have to check start->right is not null
+		insert_node(data, start->right);
+	}
+}
+
+int main() {
+	int choice;
+
+	do {
+		printf("make a selection...\n");
+		printf("	1. insert node in the tree\n	2. delete a node from the tree\n	3. search a node in tree\n	4: traverse the tree\n	99. exit the program\n");
+		scanf("%d", &choice);
+
+		switch(choice) {
+			case 1:
+				break;
+
+			case 2:
+				break;
+
+			case 3:
+				break;
+
+			default:
+				break;
+		}
+
+	}while(choice != 99);
+	
+	return 0;
+}
---
+++ general/seg_fault_example.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void main() {
+	int i = 0;
+	int arr[5];
+	for(i = 0; i<=10; i++) {
+		arr[i] = i;
+		printf("%d\n", arr[i]);
+	}
+}
